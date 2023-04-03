3274f4580539208378a9ead6adc8722e810873d1
Charles Eduardo <charleseduardofaria@gmail.com>
2023-04-03 20:56:33 -0300

    Usando imagens

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class AnthemsApp extends StatefulWidget {
 
 class _AnthemsAppState extends State<AnthemsApp> {
   var _anthemIndex = 0;
+  var _shieldIndex = 0;
 
   final Map anthems = {
     "Flamengo": "Uma vez Flamengo, sempre Flamengo. Flamengo eu sempre hei de ser",
@@ -16,6 +17,13 @@ class _AnthemsAppState extends State<AnthemsApp> {
     "Vasco": "Vamos todos cantar de coração. A cruz de malta é o meu pendão",
     "Botafogo": "Botafogo, Botafogo, campeão, desde 1910",
   };
+
+  final List shield = [
+    'https://imagepng.org/wp-content/uploads/2018/02/escudo-flamengo.png',
+    'https://upload.wikimedia.org/wikipedia/commons/a/ad/Fluminense_FC_escudo.png',
+    'https://static.dicionariodesimbolos.com.br/upload/60/81/conheca-o-significado-do-escudo-do-vasco-da-gama-5_xl.png',
+    'https://upload.wikimedia.org/wikipedia/commons/c/cb/Escudo_Botafogo.png',
+  ];
 
   void _nextAnthem() {
     setState(() {
@@ -35,6 +43,36 @@ class _AnthemsAppState extends State<AnthemsApp> {
         _anthemIndex = anthems.length - 1;
       }
     });
+  }
+
+  void _nextShield() {
+    setState(() {
+      if (_shieldIndex < shield.length - 1) {
+        _shieldIndex++;
+      } else {
+        _shieldIndex = 0;
+      }
+    });
+  }
+
+  void _previousShield() {
+    setState(() {
+      if (_shieldIndex > 0) {
+        _shieldIndex--;
+      } else {
+        _shieldIndex = shield.length - 1;
+      }
+    });
+  }
+
+  void _next() {
+    _nextAnthem();
+    _nextShield();
+  }
+
+  void _previous() {
+    _previousAnthem();
+    _previousShield();
   }
 
   @override
@@ -68,6 +106,15 @@ class _AnthemsAppState extends State<AnthemsApp> {
                   textAlign: TextAlign.center,
                 ),
               ),
+              Container(
+                margin: EdgeInsets.only(bottom: 20),
+                child: FadeInImage.assetNetwork(
+                  placeholder: 'assets/loading.gif',
+                  image: shield.elementAt(_shieldIndex),
+                  width: 200,
+                  height: 200,
+                ),
+              ),
             ],
           ),
         ),
@@ -76,7 +123,7 @@ class _AnthemsAppState extends State<AnthemsApp> {
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: <Widget>[
               ElevatedButton.icon(
-                onPressed: _previousAnthem,
+                onPressed: _previous,
                 icon: Icon(Icons.arrow_back),
                 label: Text('Anterior')
               ),
@@ -88,7 +135,7 @@ class _AnthemsAppState extends State<AnthemsApp> {
               Directionality(
                 textDirection: TextDirection.rtl,
                 child: ElevatedButton.icon(
-                    onPressed: _nextAnthem,
+                    onPressed: _next,
                     label: Text('Próximo'),
                     icon: Icon(Icons.arrow_back)
                 ),
